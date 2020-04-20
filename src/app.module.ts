2d1660d3a80bc60b8480d656566060d4bab2dec4
Stanislas Lange <angristan@pm.me>
2020-04-20 23:34:49 +0200

    Use pino for request logging
    And remove custom logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { LoggerMiddleware } from './logger.middleware';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
@@ -18,14 +18,14 @@ import { LoggerMiddleware } from './logger.middleware';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        prettyPrint: process.env.NODE_ENV !== 'production',
+      }
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes('/');
-  }
-}
+export class AppModule { }
