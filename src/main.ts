3da0d37504cdc3f2889422521dc41894a1c3a7d9
Stanislas Lange <angristan@pm.me>
2020-04-20 22:06:30 +0200

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+  .setTitle('Soundbase API')
+  .setDescription('The soundbase API description')
+  .setVersion('0.1')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
