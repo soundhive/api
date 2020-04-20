6aa5e2928b014e09c15b37b7e0bd6b66f7c01a4b
Stanislas Lange <angristan@pm.me>
2020-04-20 23:36:22 +0200

    User: update entity and fix service

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -6,10 +6,10 @@ export class User {
   id: number;
 
   @Column()
-  firstName: string;
+  name: string;
 
   @Column()
-  lastName: string;
+  username: string;
 
   @Column()
   password: string;
