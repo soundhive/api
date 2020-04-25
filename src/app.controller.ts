2a9eb38e0c1b889b462c66a2fcd7070d7c932567
Stanislas Lange <angristan@pm.me>
2020-04-26 00:48:17 +0200

    feat(app): add hello world

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private appService: AppService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
@@ -18,5 +22,10 @@ export class AppController {
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
+  }
+
+  @Get('/')
+  getRoot() {
+    return this.appService.getHello();
   }
 }
