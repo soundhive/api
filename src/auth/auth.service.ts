5ee143d73c766f0e6100062fe3ae4735b3841d5a
Stanislas Lange <angristan@pm.me>
2020-04-26 20:47:25 +0200

    feat(login): return user after login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,6 +21,7 @@ export class AuthService {
   async login(user: any) {
     const payload = { username: user.username, sub: user.userId };
     return {
+      ...user,
       "access_token": this.jwtService.sign(payload),
     };
   }
