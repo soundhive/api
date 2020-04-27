c0bf9f54b09d94f28137709de6f46f45e7bfbfc1
Stanislas Lange <angristan@pm.me>
2020-04-27 10:27:12 +0200

    feat(user): serialize user with class-transformer
    Do not return password

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
@@ -12,6 +13,7 @@ export class User {
   username: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @Column({ default: true })
