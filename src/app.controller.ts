6181c9958617ef076c248da649e3d1a09535a41f
Stanislas Lange <angristan@pm.me>
2020-05-16 13:01:17 +0200

    auth: improve safety

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,25 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthUserDTO } from './auth/dto/auth-user.dto';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
-import { AppService } from './app.service';
+import { User } from './users/user.entity';
 
 @Controller()
 export class AppController {
   constructor(
     private authService: AuthService,
     private appService: AppService,
-  ) {}
+  ) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
-    // Auth ok, return JWT token
-    return this.authService.login(req.user);
+  login(@Body() authUserDTO: AuthUserDTO) {
+    return this.authService.login(new User(authUserDTO)).then((token) => {
+      return token;
+    })
   }
 
   @UseGuards(JwtAuthGuard)
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import { User } from 'src/users/user.entity';
 import argon2 = require('argon2');
+import { User } from 'src/users/user.entity';
+
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
-  ) {}
+  ) { }
 
   async validateUser(username: string, pass: string): Promise<User> {
     const user = await this.usersService.findOne(username);
@@ -19,10 +20,9 @@ export class AuthService {
     return null;
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+  async login(user: User) {
+    const payload = { username: user.username, sub: user.id };
     return {
-      ...user,
       "access_token": this.jwtService.sign(payload),
     };
   }
