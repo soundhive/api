fb11cc5fc93c24d7749b64de3d572df5bebd91b2
Stanislas Lange <angristan@pm.me>
2020-05-17 01:55:09 +0200

    Add FindUserDTO

---
+++ src/users/dto/find-user.dto.ts
@@ -0,0 +1,9 @@
+import { IsString } from "class-validator";
+import { Exists } from "../../validators/exists.validation"
+import { User } from "../user.entity";
+
+export class FindUserDTO {
+    @IsString()
+    @Exists(User)
+    username: string
+}
