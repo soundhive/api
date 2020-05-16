6fc60a45611e07628ed1ebcb3a3db30f7bc1ea0a
Stanislas Lange <angristan@pm.me>
2020-05-17 01:54:08 +0200

    Add validation to CreateTrackDTO

--- src/tracks/dto/create-track.dto.ts
+++ src/tracks/dto/create-track.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateTrackDTO {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsString()
   genre: string;
+
+  @IsNotEmpty()
+  @IsString()
   filename: string;
 }
