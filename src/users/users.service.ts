62241baa75269a770b6a58911f78eab413225ccd
Stanislas Lange <angristan@pm.me>
2020-05-16 12:29:12 +0200

    fix: missing import in user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
