1604a559958260423e5d1ff48cb3ff9105683140
Stanislas Lange <angristan@pm.me>
2020-05-17 19:16:43 +0200

    UsersModule: cleanup imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+
 import { User } from './user.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
