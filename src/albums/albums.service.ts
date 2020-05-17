9a485e754912738302c20d7555715338c78589dd
Stéphane <rabena.stephane@gmail.com>
2020-05-17 22:58:26 +0200

    refactor(albums): rename albums service methods

--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,44 +1,32 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { Album } from './album.entity';
 import { CreateAlbumDTO } from './dto/create-album.dto';
+import { FindAlbumDTO } from './dto/find-album.dto';
+import { UpdateAlbumDTO } from './dto/update-album.dto';
 
 @Injectable()
 export class AlbumsService {
   constructor(@InjectRepository(Album) private albumsRepository: Repository<Album>) {}
 
-  async insertAlbum(createAlbumDTO: CreateAlbumDTO) {
+  async create(createAlbumDTO: CreateAlbumDTO) {
     return await this.albumsRepository.save(createAlbumDTO);
   }
 
-  async getAlbum(id: string): Promise<Album> {
-    return this.findAlbumById(id);
-  }
-  
-  async getAllAlbums(): Promise<Album[]> {
+  async find(): Promise<Album[]> {
     return await this.albumsRepository.find();
   }
-
-  // async getAlbum(id: string): Promise<Album> {
-  //   return await this.albumsRepository.findOne(id);
-  // }
-
-  async updateAlbum(id: string, album: Album): Promise<UpdateResult> {
-    return await this.albumsRepository.update(id, album);
+  
+  async findOne(album: FindAlbumDTO): Promise<Album> {
+    return await this.albumsRepository.findOne({ id: album.id });
   }
 
-  async deleteAlbum(id: string): Promise<DeleteResult> {
-    return await this.albumsRepository.delete(id);
+  async update(album: FindAlbumDTO, albumData: UpdateAlbumDTO): Promise<UpdateResult> {
+    return await this.albumsRepository.update({ id: album.id }, albumData);
   }
 
-  private async findAlbumById(id: string): Promise<Album> {
-    const found = await this.albumsRepository.findOne(id);
-
-    if (!found) {
-      throw new NotFoundException(`Album with id ${id} not found.`);
-    }
-
-    return found;
+  async delete(album: FindAlbumDTO): Promise<DeleteResult> {
+    return await this.albumsRepository.delete(album);
   }
 }
