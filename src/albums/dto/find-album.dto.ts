64e6a5a3f8cb8914e7055881b153aac94cd57a15
Stéphane <rabena.stephane@gmail.com>
2020-05-17 22:58:50 +0200

    refactor(albums): create find-album DTO

---
+++ src/albums/dto/find-album.dto.ts
@@ -0,0 +1,9 @@
+import { IsUUID } from 'class-validator';
+import { Exists } from '../../validators/exists.validation';
+import { Album } from '../album.entity';
+
+export class FindAlbumDTO {
+  @IsUUID('all')
+  @Exists(Album)
+  id: string;
+}
