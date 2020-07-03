ee85c069488da997377302dfae9186c3aebb6275
Stanislas Lange <stanislas.lange@pm.me>
2020-07-03 22:05:55 +0200

    remove spec files

--- src/app.controller.spec.ts
+++
@@ -1,9 +0,0 @@
-import { AppController } from './app.controller';
-
-describe('AppController', () => {
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(AppController.getRoot()).toBe('Hello World!');
-    });
-  });
-});
