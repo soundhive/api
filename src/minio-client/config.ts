351fb45de1bf39decc45399620e862aa04d58e58
Stanislas Lange <stanislas.lange@pm.me>
2020-07-13 18:54:05 +0200

    Minio: fix config for external S3 service + use dotenv

--- src/minio-client/config.ts
+++ src/minio-client/config.ts
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 export const config = {
-  MINIO_ENDPOINT: 'localhost',
-  MINIO_PORT: 9000,
-  MINIO_ACCESSKEY: 'miniokey',
-  MINIO_SECRETKEY: 'miniosecret',
-  MINIO_BUCKET: 'soundhive',
+  MINIO_ENDPOINT: process.env.MINIO_ENDPOINT || 'localhost',
+  MINIO_PORT: Number(process.env.MINIO_PORT) || 9000,
+  MINIO_ACCESSKEY: process.env.MINIO_ACCESSKEY || 'miniokey',
+  MINIO_SECRETKEY: process.env.MINIO_SECRETKEY || 'miniosecret',
+  MINIO_BUCKET: process.env.MINIO_BUCKET || 'soundhive',
+  MINIO_REGION: process.env.MINIO_REGION || '',
+  MINIO_USESSL: process.env.MINIO_USESSL === 'true' || false,
 };
