ae0af89e1e75cd60c103046e3b608e4c97659272
Stanislas Lange <stanislas.lange@pm.me>
2020-07-21 13:47:51 +0200

    Format migration

--- src/migrations/1595331824801-TicketClosed.ts
+++ src/migrations/1595331824801-TicketClosed.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class TicketClosed1595331824801 implements MigrationInterface {
-    name = 'TicketClosed1595331824801'
+  name = 'TicketClosed1595331824801';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "ticket" ADD "closed" boolean NOT NULL DEFAULT false`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "ticket" ADD "closed" boolean NOT NULL DEFAULT false`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "ticket" DROP COLUMN "closed"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "ticket" DROP COLUMN "closed"`);
+  }
 }
